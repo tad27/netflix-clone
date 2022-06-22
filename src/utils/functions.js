305166abd114dbc5b6d357a67ecce23b89eaf2e9
unknown <hldana2014@gmail.com>
2022-06-22 07:45:56 -0700

    Add a truncate function

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,3 +1,7 @@
 export function randomize(value) {
   return Math.floor(Math.random() * value);
 }
+
+export function truncate(str, size) {
+  return str?.length > size ? `${str.slice(0, size)}...` : str;
+}
